874c2bd3905b09c765873dc712d79e89b0bc09ef
Lionel Alliaj <la92@afsconnect2.njit.edu>
2017-02-26 21:35:20 -0500

    database.php file

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+	$dsn = 'mysql:host=sql2.njit.edu;dbname=la92';
+	$username = 'la92';
+	$password = '06CLHiUFj';
+
+	try{
+		$db = new PDO($dsn, $username, $password);
+	}
+	catch(PDOException $e){
+		$error_message = $e->getMessage();
+		include('database_error.php');
+		exit();
+	}
+?>
